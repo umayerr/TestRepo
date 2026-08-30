06c7034eceafd4bdfcf9c713626569e40d0646d1
umayerr <umayer90@gmail.com>
2026-08-30 00:51:18 -0400

    comment added

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
 def greetings(name):
     print(f'Hello {name}, welcome to the tiny world!')
 
+# main function
 def main():
    name = 'Hannah'
    greetings(name)
-        
+
 if __name__ == '__main__':
     main()
