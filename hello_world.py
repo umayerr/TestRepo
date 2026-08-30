ad30f5eb807be1ddf194f49c073606f8fedf60b7
umayerr <umayer90@gmail.com>
2026-08-29 23:50:43 -0400

    main function added

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,9 @@
 def greetings(name):
-  print(f'Hello {name}, welcome to the tiny world!')
+    print(f'Hello {name}, welcome to the tiny world!')
 
+def main():
+   name = 'Hannah'
+   greetings(name)
+        
 if __name__ == '__main__':
-  name = 'Hannah'
-  greetings(name)
+    main()
