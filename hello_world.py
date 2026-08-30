c05b104b3e8ec5b70a023efdcca9bd964ea92557
umayerr <umayer90@gmail.com>
2026-08-30 02:13:25 -0400

    Revert "comment added"
    This reverts commit 06c7034eceafd4bdfcf9c713626569e40d0646d1.

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,9 @@
 def greetings(name):
     print(f'Hello {name}, welcome to the tiny world!')
 
-# main function
 def main():
    name = 'Hannah'
    greetings(name)
-
+        
 if __name__ == '__main__':
     main()
